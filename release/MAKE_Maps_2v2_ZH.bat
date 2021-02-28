206a99e3a71afba01392d8d96740070c02ff25b4
xezon <4720891+xezon@users.noreply.github.com>
2021-02-28 13:05:43 +0100

    Remove several unpopular Ranked Maps from GenTool auto download

--- release/MAKE_Maps_2v2_ZH.bat
+++ release/MAKE_Maps_2v2_ZH.bat
@@ -2,7 +2,6 @@
 echo off
 set FILENAME=Maps_2v2_ZH.zip
 set MAPS=
-set MAPS=%MAPS% "maps_2v2_zh/[RANK] Christmas ZH v3/*"
 set MAPS=%MAPS% "maps_2v2_zh/[RANK] Conflict Zone ZH v2/*"
 set MAPS=%MAPS% "maps_2v2_zh/[RANK] Desert Combat ZH v2/*"
 set MAPS=%MAPS% "maps_2v2_zh/[RANK] FE NoBug ZH v1/*"
