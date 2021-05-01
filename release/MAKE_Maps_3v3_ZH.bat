adca968d66345a4654d29e80a9db8fc3ac9cd508
xezon <4720891+xezon@users.noreply.github.com>
2021-05-01 10:08:48 +0200

    Add map release fun [AOD] Cobalt Rush v2

--- release/MAKE_Maps_3v3_ZH.bat
+++ release/MAKE_Maps_3v3_ZH.bat
@@ -12,7 +12,7 @@ set MAPS=%MAPS% "maps_3v3_zh/Tournament Graveyard ZH v2/*"
 set MAPS=%MAPS% "maps_3v3_zh/Transition ZH v2/*"
 set MAPS=%MAPS% "maps_3v3_zh/Unity B ZH v2/*"
 ::2020
-set MAPS=%MAPS% "maps_fun_zh/[AOD] Cobalt Rush/*"
+::OBSOLETE2021 set MAPS=%MAPS% "maps_fun_zh/[AOD] Cobalt Rush/*"
 ::2021
 set MAPS=%MAPS% "maps_3v3_zh/[RANK] Coral Islands ZH v1/*"
 set MAPS=%MAPS% "maps_3v3_zh/[RANK] Defcon Balanced ZH v1/*"
@@ -22,6 +22,7 @@ set MAPS=%MAPS% "maps_3v3_zh/[RANK] Subjugation ZH v1/*"
 set MAPS=%MAPS% "maps_3v3_zh/[RANK] Territorial Dispute ZH v1/*"
 set MAPS=%MAPS% "maps_4v4_zh/[RANK] Dusty Rampage ZH v1/*"
 set MAPS=%MAPS% "maps_4v4_zh/[RANK] Twister Land ZH v1/*"
+set MAPS=%MAPS% "maps_fun_zh/[AOD] Cobalt Rush v2/*"
 set MAPS=%MAPS% "maps_fun_zh/Mortal Temptation ZH v1/*"
 
 echo on
