673a9f346077839d78fe0e9f5035517c1f63e452
xezon <4720891+xezon@users.noreply.github.com>
2021-05-01 11:36:24 +0200

    Rename map candidate 1v1 Tracks of Tragedy

--- candidate/RENAME_Maps.bat
+++ candidate/RENAME_Maps.bat
@@ -1,8 +1,1 @@
-:: 1v1
-call RENAME_From_To.bat "Tracks of Tragedy" "[RANK] A New Tradegy ZH v1"
-call RENAME_From_To.bat "Drallim Desert v2" "[RANK] Drallim Desert ZH v2"
-:: 2v2
-:: 3v3
-call RENAME_From_To.bat "Philippine Highlands" "[RANK] Philippine Highlands ZH v1"
-:: 4v4
-:: fun
+::call RENAME_From_To.bat "Tracks of Tragedy" "[RANK] A New Tragedy ZH v1"
