0101a8c34b385bebbfc7b5ccf47e737f67db310d
xezon <4720891+xezon@users.noreply.github.com>
2023-12-22 18:54:13 +0100

    Remove obsolete batch scripts

--- candidate/RENAME_From_To.bat
+++
@@ -1,9 +0,0 @@
-set NameOriginal=%~1
-set NameNew=%~2
-
-for %%i in (maps_1v1_zh,maps_2v2_zh,maps_3v3_zh,maps_4v4_zh,maps_fun_zh) do (
-	ren "%%i\%NameOriginal%\%NameOriginal%.map" "%NameNew%.map"
-	ren "%%i\%NameOriginal%\%NameOriginal%.tga" "%NameNew%.tga"
-	ren "%%i\%NameOriginal%\%NameOriginal%.wak" "%NameNew%.wak"
-	ren "%%i\%NameOriginal%" "%NameNew%"
-)
